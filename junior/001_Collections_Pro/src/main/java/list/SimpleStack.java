b86a08ad43f909cbd030d6d26830611bfbd2725e
ivanustinov <inanustinov1985@yandex.ru>
2018-04-27 11:24:33 +0500

    5.3.3

--- junior/001_Collections_Pro/src/main/java/list/SimpleStack.java
+++ junior/001_Collections_Pro/src/main/java/list/SimpleStack.java
@@ -25,46 +25,4 @@ public class SimpleStack<E> extends MyLinkedList<E> {
         remove(obj);
         return obj;
     }
-
-
-    @Override
-    public Iterator<E> iterator() {
-        return new Iterator<E>() {
-            int expectedModCount = getModCount();
-            Node<E> inststance = last;
-
-            @Override
-            public boolean hasNext() {
-                boolean result = false;
-                Node<E> localInst = inststance;
-                if (expectedModCount != getModCount()) {
-                    throw new ConcurrentModificationException();
-                }
-                while (localInst != null) {
-                    if (localInst.item == null) {
-                        localInst = localInst.prev;
-                        continue;
-                    } else {
-                        result = true;
-                        break;
-                    }
-                }
-                return result;
-            }
-
-            @Override
-            public E next() {
-                if (expectedModCount != getModCount()) {
-                    throw new ConcurrentModificationException();
-                }
-                if (inststance != null) {
-                    E e = inststance.item;
-                    inststance = inststance.prev;
-                    return e != null ? e : next();
-                } else {
-                    throw new NoSuchElementException();
-                }
-            }
-        };
-    }
 }
