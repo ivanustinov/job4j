44a6c412ffa9d2403d6d8f6881c807ddf1cb04f8
ivanustinov <inanustinov1985@yandex.ru>
2018-04-30 11:45:25 +0500

    1. Создать модель User.

---
+++ junior/001_Collections_Pro/src/main/java/map/User.java
@@ -0,0 +1,28 @@
+package map;
+
+import java.util.Calendar;
+
+/**
+ * //TODO add comments.
+ *
+ * @author Ivan Ustinov(ivanustinov1985@yandex.ru)
+ * @version 1.0
+ * @since 29.04.2018
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        String brth = birthday.get(Calendar.YEAR) + " " + birthday.get(Calendar.MONTH) + " " + birthday.get(Calendar.DATE);
+        return name + " " + children + " " + brth + " " + hashCode();
+    }
+}
