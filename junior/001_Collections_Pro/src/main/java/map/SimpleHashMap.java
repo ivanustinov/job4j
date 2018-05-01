2375844deff5014ddc154455e831f4bf35852d59
ivanustinov <inanustinov1985@yandex.ru>
2018-05-01 19:18:21 +0500

    8. Реализовать собчтвенную структуру данных.

---
+++ junior/001_Collections_Pro/src/main/java/map/SimpleHashMap.java
@@ -0,0 +1,154 @@
+package map;
+
+import java.util.*;
+
+/**
+ * //TODO add comments.
+ *
+ * @author Ivan Ustinov(ivanustinov1985@yandex.ru)
+ * @version 1.0
+ * @since 01.05.2018
+ */
+public class SimpleHashMap<K, V> implements Iterable {
+    private Entry<?, ?>[] table = new Entry[11];
+    private int modCount = 0;
+    private int count;
+    private float loadFactor = 0.75f;
+    private int threshold = (int) (table.length * loadFactor);
+
+    @Override
+    public Iterator<V> iterator() {
+        return new Iterator<V>() {
+            int expectedModCount = modCount;
+            int newInst = 0;
+            int ifElse = count;
+
+            @Override
+            public boolean hasNext() {
+                boolean result = false;
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (ifElse > 0) {
+                    result = true;
+                }
+                return result;
+            }
+
+            @Override
+            public V next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                for (int i = newInst; i < table.length; i++) {
+                    if (table[i] != null) {
+                        newInst = i;
+                        ifElse--;
+                        return (V) table[newInst++].value;
+                    } else continue;
+                }
+                throw new NoSuchElementException();
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(table);
+    }
+
+    public int tableSize() {
+        return table.length;
+    }
+
+    private static class Entry<K, V> {
+        final int hash;
+        final K key;
+        V value;
+
+        protected Entry(int hash, K key, V value) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+        }
+
+        public int hashCode() {
+            return hash ^ Objects.hashCode(value);
+        }
+
+        public String toString() {
+            return key.toString() + "=" + value.toString();
+        }
+    }
+
+    public boolean insert(K key, V value) {
+        // Make sure the value is not null
+        if (value == null) {
+            throw new NullPointerException();
+        }
+        // Makes sure the key is not already in the hashtable.
+        int hash = key.hashCode();
+        int index = (hash & 0x7FFFFFFF) % table.length;
+        if (table[index] != null) {
+            return false;
+        } else {
+            addEntry(hash, key, value, index);
+            return true;
+        }
+    }
+
+    private void addEntry(int hash, K key, V value, int index) {
+        modCount++;
+        if (count >= threshold) {
+            // Rehash the table if the threshold is exceeded
+            rehash();
+            index = (hash & 0x7FFFFFFF) % table.length;
+        }
+        // Creates the new entry.
+        table[index] = new Entry<>(hash, key, value);
+        count++;
+    }
+
+
+    protected void rehash() {
+        int oldCapacity = table.length;
+        Entry<?, ?>[] oldMap = table;
+        int newCapacity = (oldCapacity << 1) + 1;
+        Entry<?, ?>[] newMap = new Entry<?, ?>[newCapacity];
+        modCount++;
+        threshold = (int) (newCapacity * loadFactor);
+        table = newMap;
+        for (int i = oldCapacity; i-- > 0; ) {
+            Entry<K, V> old = (Entry<K, V>) oldMap[i];
+            if (old != null) {
+                int index = (old.hash & 0x7FFFFFFF) % newCapacity;
+                newMap[index] = old;
+            }
+        }
+    }
+
+    public synchronized V get(Object key) {
+        int hash = key.hashCode();
+        int index = (hash & 0x7FFFFFFF) % table.length;
+        Entry<?, ?> e = table[index];
+        if (e != null) {
+            if ((e.hash == hash) && e.key.equals(key)) {
+                return (V) e.value;
+            }
+        }
+        return null;
+    }
+
+    public synchronized boolean remove(Object key) {
+        int hash = key.hashCode();
+        int index = (hash & 0x7FFFFFFF) % table.length;
+        Entry<K, V> e = (Entry<K, V>) table[index];
+        if (e != null) {
+            if ((e.hash == hash) && e.key.equals(key)) {
+                table[index] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ junior/001_Collections_Pro/src/test/java/map/SimpleHashMapTest.java
@@ -0,0 +1,62 @@
+package map;
+
+import org.junit.Before;
+import org.junit.Test;
+import set.SimpleHash;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * //TODO add comments.
+ *
+ * @author Ivan Ustinov(ivanustinov1985@yandex.ru)
+ * @version 1.0
+ * @since 01.05.2018
+ */
+public class SimpleHashMapTest {
+    SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+
+    @Before
+    public void setUp() throws Exception {
+        map.insert(15, "Ivan");
+        map.insert(16, "Grigorii");
+        map.insert(17, "Gosha");
+        map.insert(40, "Gosha");
+        map.insert(0, "Alex");
+        map.insert(3, "Maga");
+        map.insert(5, "Maga");
+        map.insert(8, "Avdotia");
+        map.insert(1, "Maga");
+    }
+
+    @Test
+    public void tryingToTest() {
+        assertThat(map.get(15), is("Ivan"));
+        assertThat(map.tableSize(), is(11));
+        map.insert(15, "Maga");
+        System.out.println(map);
+        assertThat(map.remove(15), is(true));
+        map.insert(2, "Iosif");
+        map.insert(19, "Iosif");
+        assertThat(map.tableSize(), is(23));
+        System.out.println(map);
+    }
+
+    @Test
+    public void testIterator() {
+        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+        map.insert(15, "Ivan");
+        map.insert(16, "Grigorii");
+        map.insert(17, "Gosha");
+        System.out.println(map);
+        Iterator<String> it = map.iterator();
+        assertThat(it.next(), is("Ivan"));
+        assertThat(it.next(), is("Grigorii"));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("Gosha"));
+        assertThat(it.hasNext(), is(false));
+    }
+}
