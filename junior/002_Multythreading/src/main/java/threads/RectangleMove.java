14f48809f6f1c86e4c0d02faadca6636d332aea7
ivanustinov <inanustinov1985@yandex.ru>
2018-05-21 10:39:14 +0500

    2. Реализовать механизм программной остановки потока

---
+++ junior/002_Multythreading/src/main/java/threads/RectangleMove.java
@@ -0,0 +1,44 @@
+package threads;
+
+/**
+ * @author Ivan Ustinov(ivanustinov1985@yandex.ru)
+ * @version 1.0
+ * @since 17.05.2018
+ */
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+    private boolean isContinue = true;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+
+    public void setisContinue() {
+        isContinue = false;
+    }
+
+    @Override
+    public void run() {
+        int x = 1;
+        int y = 1;
+        while (isContinue) {
+            rect.setX(rect.getX() + x);
+            rect.setY(rect.getY() + y);
+            if (rect.getX() == 300 || rect.getX() == 0) {
+                x *= -1;
+            }
+            if (rect.getY() == 300 || rect.getY() == 0) {
+                y *= -1;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
