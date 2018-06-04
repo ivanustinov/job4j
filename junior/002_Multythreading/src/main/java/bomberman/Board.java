c3caa40a9d8ecc9699606d744d5fb72f65206f63
ivanustinov <inanustinov1985@yandex.ru>
2018-06-04 15:54:15 +0500

    ThreadPool

--- junior/002_Multythreading/src/main/java/bomberman/Board.java
+++ junior/002_Multythreading/src/main/java/bomberman/Board.java
@@ -9,77 +9,86 @@ import java.util.concurrent.locks.ReentrantLock;
  * @version 1.0
  * @since 31.05.2018
  */
-public class Board {
-    private final static ReentrantLock[][] LAND = new Cell[5][5];
-    private static int[] t = new int[]{-1, 0, 1, 0};
-    private static int[] r = new int[]{0, 1, 0, -1};
+public class Board implements Runnable {
+    private static final Cell[][] land = new Cell[5][5];
+    private static final int[] t = new int[]{-1, 0, 1, 0};
+    private static final int[] r = new int[]{0, 1, 0, -1};
 
-    static {
-        for (int i = 0; i < LAND.length; i++) {
-            for (int j = 0; j < LAND[i].length; j++) {
-                LAND[i][j] = new Cell(i, j);
+    public Board() {
+        for (int i = 0; i < land.length; i++) {
+            for (int j = 0; j < land[i].length; j++) {
+                land[i][j] = new Cell(i, j);
             }
         }
     }
 
-    public static boolean checkStep(int x, int y, int stepX, int stepY) {
+    public boolean checkStep(int x, int y, int stepX, int stepY) {
         x += stepX;
         y += stepY;
-        if ((x < LAND.length && x >= 0) && (y < LAND.length && y >= 0)) {
+        if ((x < land.length && x >= 0) && (y < land.length && y >= 0)) {
             return true;
         } else {
             return false;
         }
     }
 
-    public static ReentrantLock getNextCell(ReentrantLock cell) {
+    public Cell getNextCell(ReentrantLock cell) {
         int i = (int) (Math.random() * 4);
         Cell myCell = (Cell) cell;
         int x = myCell.getX();
         int y = myCell.getY();
-        if (Board.checkStep(x, y, t[i], r[i])) {
+        if (checkStep(x, y, t[i], r[i])) {
             x += t[i];
             y += r[i];
-            return LAND[x][y];
+            return land[x][y];
         } else {
             return getNextCell(cell);
         }
     }
 
-    public static ReentrantLock create() {
+    public Cell create() {
         int x = (int) (Math.random() * 5);
         int y = (int) (Math.random() * 5);
-        return LAND[x][y];
+        return land[x][y];
     }
 
-    public static boolean move(ReentrantLock sourse, ReentrantLock dist) {
-        while (Thread.currentThread().isInterrupted()) {
+    public boolean move(Cell sourse, Cell dist) {
+        while (!Thread.currentThread().isInterrupted()) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
             System.out.println(sourse);
-            sourse.unlock();
             while (!dist.tryLock()) {
                 dist = getNextCell(sourse);
             }
+            sourse.unlock();
             move(dist, getNextCell(dist));
         }
         return Thread.currentThread().isInterrupted();
     }
 
-    public static void main(String[] args) {
-        Thread move = new Thread() {
-            @Override
-            public void run() {
-                ReentrantLock firstCell = Board.create();
-                while (!firstCell.tryLock()) {
-                    firstCell = getNextCell(firstCell);
-                }
-                Board.move(firstCell, getNextCell(firstCell));
-            }
-        };
-        move.start();
+    @Override
+    public void run() {
+        Cell firstCell = create();
+        while (!firstCell.tryLock()) {
+            firstCell = getNextCell(firstCell);
+        }
+        move(firstCell, getNextCell(firstCell));
     }
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        Thread move = new Thread(board);
+        move.start();
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        move.interrupt();
+    }
+
+
 }
