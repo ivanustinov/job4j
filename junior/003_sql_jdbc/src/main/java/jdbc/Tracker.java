bc523c24bf4571ca545d8eee1ab7cfac646e5d23
ivanustinov <inanustinov1985@yandex.ru>
2018-06-29 14:38:22 +0500

    Tracker

--- junior/003_sql_jdbc/src/main/java/jdbc/Tracker.java
+++ junior/003_sql_jdbc/src/main/java/jdbc/Tracker.java
@@ -1,98 +1,156 @@
 package jdbc;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.sql.*;
+import java.util.HashMap;
 
 /**
- * //TODO add comments.
- *
  * @author Ivan Ustinov(ivanustinov1985@yandex.ru)
  * @version 1.0
  * @since 27.06.2018
  */
 public class Tracker implements AutoCloseable {
-    //JDBC URL, username and PASSWORD of MySQL server
-    private static final String URL;
-    private static final String USER;
-    private static final String PASSWORD;
-    private static final String FILENAME = "CreateDB";
 
-    private static final String[] NAMES = {"Ivan", "Petr", "Konstantin", "Roman"};
+    private final HashMap<String, String> conf;
+    private Connection connection;
+    private Statement checkTable;
 
-    //JDBC variables for opening and managing connection
-    private static Connection connection;
-    private static Statement stmt;
+    static class Item {
+        private int id;
+        private String name;
 
-    static {
-        String urlUserPassword = readFile(FILENAME);
-        String[] ur = urlUserPassword.split(" ");
-        URL = ur[0];
-        USER = ur[1];
-        PASSWORD = ur[2];
-        try (Connection connectio_n = DriverManager.getConnection(URL, USER, PASSWORD);
-             Statement stm_t = connectio_n.createStatement()) {
-            connection = connectio_n;
-            stmt = stm_t;
+        public Item(String name) {
+            this.name = name;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public int generateId() {
+        return (int) (Math.random() * 1000);
+    }
+
+    public Tracker(Conf config) {
+        conf = config.getConfig();
+        try {
+            connection = DriverManager.getConnection(conf.get("url"), conf.get("user"), conf.get("password"));
+            checkTable = connection.createStatement();
+            checkTable.execute(conf.get("checkTable"));
+            checkTable.execute("DELETE FROM tracker");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-//    public static String initFile() {
-//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME))) {
-//            writer.write("jdbc:postgresql://localhost:5432/trackerdb postgres password");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return FILENAME;
-//    }
 
-    public static String readFile(String fileName) {
-        StringBuilder builder = new StringBuilder();
-        String a;
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            while ((a = reader.readLine()) != null) {
-                builder.append(a);
-            }
-        } catch (IOException y) {
-            y.printStackTrace();
+    public Item add(Item item) {
+        try (PreparedStatement prp = connection.prepareStatement(conf.get("insert"))) {
+            prp.setInt(1, item.getId());
+            prp.setString(2, item.getName());
+            prp.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return builder.toString();
+        return item;
     }
 
+    public void delete(Item item) {
+        try (PreparedStatement prp = connection.prepareStatement(conf.get("delete"))) {
+            prp.setInt(1, item.getId());
+            prp.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void replace(int id, Item item) {
+        try (PreparedStatement prp = connection.prepareStatement(conf.get("update"))) {
+            prp.setString(1, item.getName());
+            prp.setInt(2, item.getId());
+            prp.setInt(3, id);
+            prp.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<String, String> findAll() {
+        HashMap<String, String> items = new HashMap<>();
+        try (Statement prp = connection.createStatement()) {
+            ResultSet rs = prp.executeQuery(conf.get("select"));
+            while (rs.next()) {
+                items.put(rs.getString("id"), rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    public HashMap<String, String> findByName(String name) {
+        HashMap<String, String> items = new HashMap<>();
+        try (PreparedStatement prp = connection.prepareStatement(conf.get("select by name"))) {
+            prp.setString(1, name);
+            ResultSet rs = prp.executeQuery();
+            while (rs.next()) {
+                items.put(rs.getString("id"), rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    public Item findById(int id) {
+        Item item = null;
+        try (PreparedStatement prp = connection.prepareStatement(conf.get("select by id"))) {
+            prp.setInt(1, id);
+            ResultSet rs = prp.executeQuery();
+            while (rs.next()) {
+                item = new Item(rs.getString("name"));
+                item.setId(Integer.valueOf(rs.getString("id")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
 
     @Override
     public void close() throws Exception {
         connection.close();
-        stmt.close();
+        checkTable.close();
     }
-
-    public void checkTable() throws SQLException {
-        stmt.execute("CREATE TABLE IF NOT EXISTS tracker(" +
-                "id SERIAL PRIMARY KEY," +
-                "name text);");
-    }
-
-
-    public void setInsertNames() {
-        try (PreparedStatement insertNames = connection.prepareStatement("INSERT INTO tracker (name) VALUES (?);")) {
-            for (String name : NAMES) {
-                insertNames.setString(1, name);
-                insertNames.addBatch();
-            }
-            insertNames.executeBatch();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     public static void main(String[] args) {
-        try (Tracker tracker = new Tracker()) {
-            tracker.checkTable();
-            tracker.setInsertNames();
+
+        try (Tracker tracker = new Tracker(new Conf())) {
+            Item first = new Item("Alex");
+            Item second = new Item("Ivan");
+            Item third = new Item("Ivan");
+            Item forth = new Item("Ivan");
+            first.setId(tracker.generateId());
+            second.setId(tracker.generateId());
+            third.setId(tracker.generateId());
+            forth.setId(tracker.generateId());
+            tracker.add(first);
+            tracker.add(second);
+            tracker.add(forth);
+            tracker.delete(second);
+            tracker.replace(first.getId(), third);
+            System.out.println(tracker.findAll());
+            System.out.println(tracker.findByName("Ivan"));
+            System.out.println(second.getId());
+            System.out.println(tracker.findById(third.getId()));
         } catch (Exception e) {
             e.printStackTrace();
         }
